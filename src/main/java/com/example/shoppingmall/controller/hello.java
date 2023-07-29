d90ded3b0ca72d12d63ad5ed4455d5a15b310c18
kimjiyoon <wldbs9435@hanmail.net>
2023-07-29 17:21:16 +0900

    FEAT: DB 연결, git ignore 설정

---
+++ src/main/java/com/example/shoppingmall/controller/hello.java
@@ -0,0 +1,14 @@
+package com.example.shoppingmall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class hello {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ src/main/java/com/example/shoppingmall/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.example.shoppingmall.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+// mapper 인터페이스
+@Mapper // spring에 bean으로 등록
+public interface CustomerRepository {
+}
